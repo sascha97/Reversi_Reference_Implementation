ddb9bbd84d4e4348d9ee5ed1cafed8b6aea95a50
sascha97 <sascha61@live.de>
2016-07-12 21:09:46 +0200

    Updated the copyright and some minor improvements

--- src/reversi/game/ReversiGameConfiguration.java
+++ src/reversi/game/ReversiGameConfiguration.java
@@ -1,9 +1,32 @@
-/**
- * (C) Copyright 2016 Sascha Lutzenberger
- * <p>
- * This file is part of the project "Reversi_Reference_Implementation".
- * <p>
- * The distribution or use of this project is prohibited.
+/*
+ * Copyright (c) 2016. Sascha Lutzenberger. All rights reserved.
+ *
+ * This file is part of the project "Reversi_Reference_Implementation"
+ *
+ * Redistribution and use in source and binary forms, without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * - The author of this source code has given you the permission to use this
+ *   source code.
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * - The code is not used in commercial projects, except you got the permission
+ *   for using the code in any commerical projects from the author.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package reversi.game;
 
--- src/reversi/ui/GraphicalGamePreferences.java
+++ src/reversi/ui/GraphicalGamePreferences.java
@@ -1,9 +1,32 @@
-/**
- * (C) Copyright 2016 Sascha Lutzenberger
- * <p>
- * This file is part of the project "Reversi_Reference_Implementation".
- * <p>
- * The distribution or use of this project is prohibited.
+/*
+ * Copyright (c) 2016. Sascha Lutzenberger. All rights reserved.
+ *
+ * This file is part of the project "Reversi_Reference_Implementation"
+ *
+ * Redistribution and use in source and binary forms, without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * - The author of this source code has given you the permission to use this
+ *   source code.
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * - The code is not used in commercial projects, except you got the permission
+ *   for using the code in any commerical projects from the author.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package reversi.ui;
 
@@ -183,7 +206,7 @@ public class GraphicalGamePreferences {
 
         //The values of the slider
         int minValue = 6;
-        int maxValue = 16;
+        int maxValue = 12;
         //Set up a new JSlider
         final JSlider sliderBoardSize = new JSlider(minValue, maxValue, currentValue);
         //Set a tick at every whole number
@@ -422,10 +445,10 @@ public class GraphicalGamePreferences {
     }
 
     /**
-     * This method is used to store the changed prefernces to the game config
+     * This method is used to store the changed preferences to the game config
      */
     private void saveChangesToReversiGameConfiguration() {
-        //Get all the setted keys of the preference map
+        //Get all the set keys of the preference map
         Set<String> keys = preferenceMap.keySet();
 
         //for each key set the value to the reversi game configuration
@@ -433,7 +456,7 @@ public class GraphicalGamePreferences {
             changeReversiGameConfiguration(key, preferenceMap.get(key));
         }
 
-        //set the flag that the config has chagned
+        //set the flag that the config has changed
         hasConfigurationChanged = true;
     }
 
